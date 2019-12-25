e8191078444ebf8eca3ad2efbcf2579bd6b05f90
lcdada <15810227932@163.com>
2019-12-25 14:36:08 +0800

    first admin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,40 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
+
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//       //这里判断用户是否登录，验证本地存储是否有token
+//       if (!localStorage.token) { // 判断当前的token是否存在
+//           next({
+//               path: '/login',
+//               query: { redirect: to.fullPath }
+//           })
+//       } else {
+//           next()
+//       }
+//   } else {
+//       next() // 确保一定要调用 next()
+//   }
+// })
+
+// router.beforeEach((to, from, next) => {
+//   if(to.path === '/login'){
+//     sessionStorage.removeItem('user');
+//   }
+//   var user = sessionStorage.getItem('user');
+//   if(!user && to.path !== '/login'){
+//     next({
+//       path: '/login'
+//     })
+//   }else{
+//     next();
+//   }
+// })
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Login from "../views/Login.vue"
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: "/home",
     name: "home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   },
   {
     path: "/about",
@@ -18,6 +28,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue")
   }
 ];
 
